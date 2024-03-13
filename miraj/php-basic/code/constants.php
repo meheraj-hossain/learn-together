90b5909a4a9d522ee39c4280a9f41a872a6a6ef1
Meheraj-Hossain <mehossain13@gmail.com>
2024-03-14 01:05:09 +0600

    on variables

---
+++ miraj/php-basic/code/constants.php
@@ -0,0 +1,34 @@
+<?php
+
+//constants
+//we use define() function to create a constant
+define("MaxSize", 100);
+
+echo MaxSize;
+echo "<br>";
+echo constant("MaxSize");
+echo "</br>";
+
+//interface One
+//{
+//    const TEXT = "This is from Interface";
+//}
+//
+//class Two
+//{
+//    const TEXT = "This is from class";
+//}
+//
+//echo Two::TEXT;
+//echo "</br>";
+//echo One::TEXT;
+//
+//define("t", "TEXT");
+//var_dump(constant('One::'.t));
+//var_dump(constant('Two::'.t));
+
+const text = "This is a ";
+const extendedText = text . ' Text';
+echo text;
+echo "</br>";
+echo extendedText;
